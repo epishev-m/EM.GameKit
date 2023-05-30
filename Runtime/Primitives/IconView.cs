d1f064e65b2b8b34eb35865c38d3ebfb838e3022
Maxim Epishev <epishev.m@mail.ru>
2023-05-31 00:45:25 +0300

    Add Primitives

---
+++ Runtime/Primitives/IconView.cs
@@ -0,0 +1,97 @@
+﻿namespace EM.GameKit
+{
+
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.U2D;
+
+public sealed class IconView : MonoBehaviour
+{
+	[SerializeField]
+	private AssetReference _assetReference;
+
+	[SerializeField]
+	private ImageView _image;
+
+	[SerializeField]
+	private PreloaderView _preloaderView;
+
+	private SpriteAtlas _spriteAtlas;
+
+	#region MonoBehaviour
+
+	private void Awake()
+	{
+		if (_spriteAtlas == null)
+		{
+			_image.enabled = false;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		CleanUp();
+	}
+
+	#endregion
+
+	#region ImageView
+
+	public bool Gray
+	{
+		get => _image.Gray;
+		set => _image.Gray = value;
+	}
+
+	public float CoverAmount
+	{
+		get => _image.CoverAmount;
+		set => _image.CoverAmount = value;
+	}
+
+	public Color CoverColor
+	{
+		get => _image.CoverColor;
+		set => _image.CoverColor = value;
+	}
+
+	public async UniTask SetImageAsync(string image)
+	{
+		await LoadSpriteAtlas();
+		var sprite = _spriteAtlas.GetSprite(image);
+		_image.SetSprite(sprite);
+		_image.enabled = true;
+	}
+
+	public void SetSize(Vector2 size)
+	{
+		if (TryGetComponent<RectTransform>(out var rectTransform))
+		{
+			rectTransform.sizeDelta = new Vector2(size.x, size.y);
+		}
+	}
+
+	public void CleanUp()
+	{
+		_image.CleanUp();
+		_spriteAtlas = null;
+		_assetReference.ReleaseAsset();
+	}
+
+	private async UniTask LoadSpriteAtlas()
+	{
+		if (_spriteAtlas != null)
+		{
+			return;
+		}
+
+		_preloaderView.Show();
+		_spriteAtlas = await _assetReference.LoadAssetAsync<SpriteAtlas>().ToUniTask();
+		_preloaderView.Hide();
+	}
+
+	#endregion
+}
+
+}
---
+++ Runtime/Primitives/ImageView.cs
@@ -0,0 +1,130 @@
+﻿namespace EM.GameKit
+{
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class ImageView : MonoBehaviour
+{
+	[SerializeField]
+	private Image _image;
+
+	private bool _isAwaked;
+
+	private Color _color = Color.white;
+
+	private Material _material;
+
+	private bool _gray;
+
+	private float _coverAmount;
+
+	private Color _coverColor;
+
+	#region MonoBehaviour
+
+	private void Awake()
+	{
+		_color = _image.color;
+		_isAwaked = true;
+	}
+
+	#endregion
+
+	#region ImageView
+
+	public bool Gray
+	{
+		get => _gray;
+		set
+		{
+			if (_gray == value)
+			{
+				return;
+			}
+
+			_gray = value;
+			CreateMaterial();
+			_material.SetFloat(ShaderConstants.Properties.GrayAmount, _gray ? 1f : 0f);
+		}
+	}
+
+	public float CoverAmount
+	{
+		get => _coverAmount;
+		set
+		{
+			if (_coverAmount == value)
+			{
+				return;
+			}
+
+			_coverAmount = value;
+			CreateMaterial();
+			_material.SetFloat(ShaderConstants.Properties.CoverAmount, value);
+		}
+	}
+
+	public Color CoverColor
+	{
+		get => _coverColor;
+		set
+		{
+			if (_coverColor == value)
+			{
+				return;
+			}
+
+			_coverColor = value;
+			CreateMaterial();
+			_material.SetColor(ShaderConstants.Properties.CoverColor, value);
+		}
+	}
+
+	public void SetSprite(Sprite sprite)
+	{
+		if (_image.sprite == sprite)
+		{
+			return;
+		}
+
+		_image.sprite = sprite;
+
+		if (_material != null)
+		{
+			_material.mainTexture = _image.mainTexture;
+		}
+	}
+
+	public void CleanUp()
+	{
+		if (_material != null)
+		{
+			Destroy(_material);
+			_material = null;
+		}
+
+		if (_isAwaked)
+		{
+			_image.color = _color;
+		}
+
+		_gray = false;
+	}
+
+	private void CreateMaterial()
+	{
+		if (_material != null)
+		{
+			return;
+		}
+
+		_material = new Material(Shader.Find(ShaderConstants.Names.CoverColor));
+		_image.material = _material;
+		_material.mainTexture = _image.mainTexture;
+	}
+
+	#endregion
+}
+
+}
