736a07ded1d7717b567b22e5620837345d923379
Maxim Epishev <epishev.m@mail.ru>
2023-05-31 00:44:13 +0300

    Add em_ui_сover_сolor.shader

---
+++ Runtime/Shaders/ShaderConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace EM.GameKit
+{
+
+using UnityEngine;
+
+public static class ShaderConstants
+{
+	public static class Names
+	{
+		public static readonly string CoverColor = "EM/UI/CoverColor";
+	}
+
+	public static class Properties
+	{
+		public static readonly int GrayAmount = Shader.PropertyToID("_GrayAmount");
+
+		public static readonly int CoverColor = Shader.PropertyToID("_CoverColor");
+
+		public static readonly int CoverAmount = Shader.PropertyToID("_CoverAmount");
+	}
+}
+
+}
