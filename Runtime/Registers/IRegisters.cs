3dc628e35cb1b4c28b64a0eb0b74082a23467fe1
Maxim Epishev <epishev.m@mail.ru>
2023-06-11 21:10:43 +0300

    Add Registers

---
+++ Runtime/Registers/IRegisters.cs
@@ -0,0 +1,19 @@
+﻿namespace EM.GameKit
+{
+
+using System;
+
+public interface IRegisters
+{
+	event Action<string, long> OnChanged;
+
+	long GetRegister(string key);
+
+	void SetRegister(string key,
+		long value);
+
+	long AddRegister(string key,
+		long delta);
+}
+
+}
---
+++ Runtime/Registers/Registers.cs
@@ -0,0 +1,40 @@
+﻿namespace EM.GameKit
+{
+
+using System;
+using System.Collections.Generic;
+
+public sealed class Registers : IRegisters
+{
+	private readonly Dictionary<string, long> _registers = new();
+
+	public event Action<string, long> OnChanged;
+
+	#region IRegisters
+
+	public long GetRegister(string key)
+	{
+		return _registers.TryGetValue(key, out var val) ? val : 0;
+	}
+
+	public void SetRegister(string key,
+		long value)
+	{
+		_registers[key] = value;
+		OnChanged?.Invoke(key, value);
+	}
+
+	public long AddRegister(string key,
+		long delta)
+	{
+		var value = GetRegister(key);
+		value += delta;
+		SetRegister(key, value);
+
+		return value;
+	}
+
+	#endregion
+}
+
+}
