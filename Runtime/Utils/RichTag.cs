29c1f2fc7611bccf26439ed09a5adb4cd9d098db
Maxim Epishev <epishev.m@mail.ru>
2023-07-11 18:51:57 +0300

    Add RichTag

---
+++ Runtime/Utils/RichTag.cs
@@ -0,0 +1,77 @@
+namespace EM.GameKit
+{
+
+using UnityEngine;
+
+public static class RichTag
+{
+	public static string RelScale(float percent, object text)
+	{
+		return $"<size={percent}%>{text}</size>";
+	}
+
+	public static string FontSize(float fontSize, object text)
+	{
+		return $"<size={fontSize}>{text}</size>";
+	}
+
+	public static string VOffset(float offset, object text)
+	{
+		// ReSharper disable once StringLiteralTypo
+		return $"<voffset={offset}om>{text}</voffset>";
+	}
+
+	public static string Style(string value)
+	{
+		return $"<style={value}>";
+	}
+		
+	public static string Bold(string value)
+	{
+		return $"<b>{value}</b>";
+	}
+
+	public static string Colored(string htmlStringRgb, object text)
+	{
+		return $"<color={htmlStringRgb}>{text}</color>";
+	}		
+		
+	public static string Colored(Color c, string text)
+	{
+		return $"<color=#{ColorUtility.ToHtmlStringRGB(c)}>{text}</color>";
+	}
+
+	public static string GrayArrow => Colored("#b5b5cb", "<sprite name=arrow>");
+		
+	public static string Number(object num)
+	{
+		return Colored("#48cd6f", num);
+	}		
+
+	public static string BigNumber(object num)
+	{
+		return RelScale(120, Colored("#48cd6f", num));
+	}		
+		
+	public static string Sprite(string value)
+	{
+		return $"<sprite name=\"{value}\">";
+	}
+		
+	public static string SpriteIndent(string value, float percent)
+	{
+		return $"<sprite name=\"{value}\"><indent={percent}%>";
+	}
+		
+	public static string BigSprite(string value, int percentSize, float offset)
+	{
+		return VOffset(offset, RelScale(percentSize, Sprite(value)));
+	}
+		
+	public static string BigSpriteIndent(string value, int percentSize, float offset, float percent)
+	{
+		return VOffset(offset, RelScale(percentSize, SpriteIndent(value, percent)));
+	}
+}
+
+}
